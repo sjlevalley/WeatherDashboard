11528953bb7fc5278b8a42f5bac24a3bc8b2b1a5
Steve LeValley <slevalley@gmail.com>
2021-03-27 15:28:30 -0700

    added some variables in JS

---
+++ script.js
@@ -0,0 +1,21 @@
+var citySearched = $('citySearched');
+var searchButton = $('searchButton');
+
+var recentSearches = $('recentSearches');
+
+var currentCity = $('currentCity');
+var currentDate = $('currentDate');
+var weatherIcon = $('weatherIcon');
+var tempText = $('tempText');
+var humidityText = $('humidityText');
+var windSpeedText = $('windSpeedText');
+var unIndexText = $('uvIndexText');
+
+var forecastDate = $('forecastDate');
+var forecastIcon = $('forecastIcon');
+var forecastTemp = $('forecastTemp');
+var forecastHumidity = $('forecastHumidity');
+
+
+
+
