5e7474dd8d9962a58e7095ce11cd2b6bbc160f82
Steve LeValley <slevalley@gmail.com>
2021-03-28 12:42:02 -0700

    All Current weather and Forecasts working but no UV index, no moment, and needs some styling

--- script.js
+++ script.js
@@ -10,9 +10,9 @@ var weatherIcon = $('.weatherIcon');
 var tempText = $('.tempText');
 var humidityText = $('.humidityText');
 var windSpeedText = $('.windSpeedText');
-var unIndexText = $('.uvIndexText');
+var uvIndexText = $('.uvIndexText');
 
-var forecastDate = $('.forecastDate');
+var forecastDate1 = $('.forecastDate1');
 var forecastIcon = $('.forecastIcon');
 var forecastTemp = $('.forecastTemp');
 var forecastHumidity = $('.forecastHumidity');
@@ -92,20 +92,71 @@ function searchAPI() {
 
     var APIkey = '8f8c2546cebab0c1b04d441c650feb01';
     var cityVal = citySearched.val();
-    var locQueryUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityVal + '&appid=' + APIkey;
+    var locQueryUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityVal + '&units=imperial' + '&appid=' + APIkey;
 
     fetch(locQueryUrl)
-    .then(response => response.json())
+    .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data)
 
-    .then(data => console.log(data)); 
+        console.log(data.list[0].wind.speed);
+        
+        currentCity.text(data.city.name);
+        currentDate.text(data.list[0].dt_txt);
+        weatherIcon.text(data.list[0].weather[0].icon);
+        tempText.text(data.list[0].main.temp + "°F");
+        humidityText.text(data.list[0].main.humidity + "%");
+        windSpeedText.text(data.list[0].wind.speed + " MPH");
+        // uvIndexText.text();
+
+
+        // ################ Forecast Days ####################
+        day1.children()[0].textContent = data.list[4].dt_txt
+        day1.children()[1].textContent = data.list[4].weather[0].icon
+        day1.children()[2].textContent = data.list[4].main.temp + "°F"
+        day1.children()[3].textContent = data.list[4].main.humidity + "%"
+
+        day2.children()[0].textContent = data.list[12].dt_txt
+        day2.children()[1].textContent = data.list[12].weather[0].icon
+        day2.children()[2].textContent = data.list[12].main.temp + "°F"
+        day2.children()[3].textContent = data.list[12].main.humidity + "%"
+
+        day3.children()[0].textContent = data.list[20].dt_txt
+        day3.children()[1].textContent = data.list[20].weather[0].icon
+        day3.children()[2].textContent = data.list[20].main.temp + "°F"
+        day3.children()[3].textContent = data.list[20].main.humidity + "%"
+
+        day4.children()[0].textContent = data.list[28].dt_txt
+        day4.children()[1].textContent = data.list[28].weather[0].icon
+        day4.children()[2].textContent = data.list[28].main.temp + "°F"
+        day4.children()[3].textContent = data.list[28].main.humidity + "%"
+
+        day5.children()[0].textContent = data.list[36].dt_txt
+        day5.children()[1].textContent = data.list[36].weather[0].icon
+        day5.children()[2].textContent = data.list[36].main.temp + "°F"
+        day5.children()[3].textContent = data.list[36].main.humidity + "%"
+
+        
+
+
+
+        
+      })
+
+      
+ 
     // .then(data => console.log(data.list[1])); Works
+    // .then(data => console.log(data.list[1])); 
     // .then(data => console.log(data.list[1].weather[0].icon)); Works
 
     
 }
 
 
-
+// forecastDate.text("Hello");
+// day1.children()[0].textContent = "Goodbye"
 
 
 // ########### AutoComplete Widget #####################
